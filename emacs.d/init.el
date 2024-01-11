87c97ba787982657062edcc134dd902f25d801bd
Ed Swierk <eswierk@apple.com>
2024-01-11 09:34:42 -0800

    Add swift-mode emacs package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -224,7 +224,7 @@ Should be added to `comint-input-filter-functions' like so:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lua-mode multiple-cursors)))
+ '(package-selected-packages '(swift-mode lua-mode multiple-cursors)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
