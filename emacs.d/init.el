8dbcba1e8493c19e1debb0b6c0abe7bac1e9bd03
Ed Swierk <eswierk@apple.com>
2024-07-11 15:01:39 -0700

    Add magit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -238,6 +238,8 @@ Should be added to `comint-input-filter-functions' like so:
 
 (column-number-mode)
 
+(require 'git-commit)
+
 ; Display line number rather than ?? even when buffer contains long
 ; lines
 (setq line-number-display-limit-width 10000)
@@ -249,7 +251,8 @@ Should be added to `comint-input-filter-functions' like so:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(vterm swift-mode lua-mode multiple-cursors)))
+ '(package-selected-packages
+   '(gnu-elpa-keyring-update magit vterm swift-mode lua-mode multiple-cursors)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
